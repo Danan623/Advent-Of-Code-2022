b11b37fdbf0649c2b6c244cbeb55cc5d1ba8f1e2
Danan623 <Andersson920131@outlook.com>
2022-12-11 14:00:22 +0100

    Day2
    Day2

--- AdventOfCode.py
+++ AdventOfCode.py
@@ -1,4 +1,3 @@
-
 class AdventOfCode:
 
     def __init__(self):
@@ -7,11 +6,12 @@ class AdventOfCode:
 
     def introduction(self):
         # Access the name of the current instance of the class
-        print(f"Hello from {self.name}!" + "\n" + "Student in Information Technology at Linköpings University" + "\n" +"My idea is to write every day in the same class (class AdventOfCode)."
-              + "\n" + "Each day's code will then be able to run through an instance variable created in the DriverClass." + "\n"*5)
+        print(
+            f"Hello from {self.name}!" + "\n" + "Student in Information Technology at Linköpings University" + "\n" + "My idea is to write every day in the same class (class AdventOfCode)."
+            + "\n" + "Each day's code will then be able to run through an instance variable created in the DriverClass." + "\n" * 5)
 
     # ---------------------------------- advent of code 2022 day 1 task 1 ------------------------------------------
-    def biggest_item(self, s: str) -> int:
+    def day1_task1(self, s: str) -> int:
 
         # store current item
         current = 0
@@ -40,7 +40,7 @@ class AdventOfCode:
         return sum(biggest)
 
     # ---------------------------------- advent of code 2022 day 1 task 2 ------------------------------------------
-    def biggest_3_items(self, s: str) -> int:
+    def day1_task2(self, s: str) -> int:
         # store current item
         current = 0
         # we will need to put the sums in a list or a dict, a list is simpler in this task
@@ -60,10 +60,113 @@ class AdventOfCode:
         # Sort the list (the highest to the lowest)
         sumList.sort(reverse=True)
         # check the list order
-        #print(sumList)
+        # print(sumList)
 
         # add the sums together of the three highest values
         return sumList[0] + sumList[1] + sumList[2]
+
+    # ---------------------------------- advent of code 2022 day 2 task 1 -------***FAIL CODE***------------------------
+    # this code will give the wrong output. I can't figure out why, but I think I will need to write in another way
+    # read from text file
+    def total_score(self, s: str) -> int:
+        # stores result
+        result = []
+        # to test if it will be something with the list, but I don't think so
+        test = 0
+        # map the values of each combat sign
+        yourvalues = {'X': 1, 'Y': 2, 'Z': 3}
+        opponentvalues = {'A': 1, 'B': 2, 'C': 3}
+
+        # win and draw scores
+        WIN_SCORE = 6
+        DRAW_SCORE = 3
+
+
+        # init score variables for the rounds
+        your_round = 0
+        opponents_round = 0
+
+        # set counter to check each round
+        counter = 0
+
+        for i in s:
+            #print(i)
+            # Check for space characters and newLine -> ignore
+            if i == ' ' or i == '\n':
+                continue
+            # will give you one string at a time after this
+
+            # look up value of input
+            if i in yourvalues:
+                your_round = yourvalues[i]
+
+            elif i in opponentvalues:
+                 opponents_round = opponentvalues[i]
+
+            # Increment counter
+            counter += 1
+            print(i)
+            # Check if both players have played a "combat sign" in the round
+            if counter == 2:
+                #print("motsåndare ", opponents_round)
+                #print("DU ", your_round)
+                # Check the winner of the round
+                if your_round > opponents_round:
+                    # you win the round = yourRound + 6 points
+                    result.append(your_round + WIN_SCORE)
+                    #test += your_round + WIN_SCORE
+                    #print("vinner")
+                elif your_round == opponents_round:
+                    # it's a draw = yourRound + 3 points
+                    result.append(your_round + DRAW_SCORE)
+                    #test += your_round + DRAW_SCORE
+                   # print("draw")
+                elif your_round < opponents_round:
+                    result.append(your_round)
+                    #test += your_round
+                    #print("förlust")
+
+                # Clear the score variables and start a new round
+                your_round = 0
+                opponents_round = 0
+                counter = 0
+                #print("resultat ", test)
+
+
+
+        # Return the sum of the result
+        return sum(result)
+
+    # ---------------------------------- advent of code 2022 day 2 task 1 ---------------------------------------------
+    # take input in a list of strings instead of one character at a time like ["A Z",...]
+    # make a table (a dict) with the scores and let the different combinations be key and the integer scores be values
+    # mapped to the different keys
+    # iterate and check each key - value pair and, store the correct match and return the result
+    def day2_task1(self, s: str) -> int:
+
+        table = {"A X":4, "A Y":8, "A Z":3, "B X":1, "B Y":5, "B Z":9,"C X":7, "C Y":2, "C Z":6}
+       # table = {"A X":3, "A Y":4, "A Z":8, "B X":1, "B Y":5, "B Z":9,"C X":2, "C Y":6, "C Z":7}
+        total = 0
+        for i in s:
+            #print(i)
+            total += table[i]
+
+        return total
+
+    # ---------------------------------- advent of code 2022 day 2 task 2 ---------------------------------------------
+    # just change the values in the table.. simple as that
+    def day2_task2(self, s: str) -> int:
+
+       # table = {"A X":4, "A Y":8, "A Z":3, "B X":1, "B Y":5, "B Z":9,"C X":7, "C Y":2, "C Z":6}
+        table = {"A X":3, "A Y":4, "A Z":8, "B X":1, "B Y":5, "B Z":9,"C X":2, "C Y":6, "C Z":7}
+        total = 0
+        for i in s:
+            #print(i)
+            total += table[i]
+
+        return total
+
+
 
 
 
--- DriverClass.py
+++ DriverClass.py
@@ -7,17 +7,34 @@ class DriverClass:
     adventOfCode = AdventOfCode()
     adventOfCode.introduction()
 
+    # ********************************** DAY 1 *******************************************
+
     # Open the file in reading mode
     with open("Day1.txt", "r") as f:
+
         # Read the contents of the file
         numbers = f.read()
+
         # Split the contents of the file into a list of strings
         numbers = numbers.split('\n')
 
-    # Call the biggest_item() function with the contents of the file as the argument
+    result = adventOfCode.day1_task1(numbers) # *********** correct ****************
+    print("advent of code 2022 day 1 task 1: biggest item is", result)
 
-    #result = adventOfCode.biggest_item(numbers)
-    #print("advent of code 2022 day 1 task 1: biggest item is", result)
-
-    result = adventOfCode.biggest_3_items(numbers)
+    result = adventOfCode.day1_task2(numbers) # *********** correct ****************
     print("advent of code 2022 day 1 task 2: biggest item is", result)
+
+    # ********************************** DAY 2 *******************************************
+
+    with open("Day2.txt", "r") as f: # 14610
+        numbers = f.read()
+
+    number = numbers.split("\n")
+    #print(number)
+
+    #result = adventOfCode.total_score(number) *********** FAIL *******************
+    result = adventOfCode.day2_task1(number) # *********** correct ****************
+    print("advent of code 2022 day 2 task 1: ", result)
+
+    result = adventOfCode.day2_task2(number)  # *********** correct ****************
+    print("advent of code 2022 day 2 task 2: ", result)
